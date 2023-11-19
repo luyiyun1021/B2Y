d2937a718409798da849fe8a144b11e6add410ea
Indra9527 <mke2@cs.brown.edu>
2023-11-19 13:33:35 -0500

    feat: add name and desc to sets

--- Frontend/src/components/B2YViewer.js
+++ Frontend/src/components/B2YViewer.js
@@ -16,133 +16,133 @@ import { VideoList } from "./VideoList";
 import { SetList } from "./SetList";
 import { RollbackOutlined } from "@ant-design/icons";
 
-let data = {
-  videos: [
-    {
-      id: "BV1LC4y1Z7Li",
-      bvid: "BV1LC4y1Z7Li",
-      aid: 789284286,
-      cid: 1291466320,
-      title: "Foggy Brown",
-      desc: "Bronw University Main Green on a foggy data",
-      img: "http://i2.hdslb.com/bfs/archive/4632e780d3411dccd8fd6758e7d39968449cebf5.jpg",
-      owner_mid: 1794123514,
-      owner_name: "robertmin96",
-      view: 6,
-      like: 0,
-      star: 0,
-      share: 0,
-      comment: 1,
-      disable: true,
-      checked: false,
-    },
-    {
-      id: "BV1y8411C7Mw",
-      bvid: "BV1y8411C7Mw",
-      aid: 234207717,
-      cid: 1285040206,
-      title: "Tennis Practice",
-      desc: "I played tennis with friend in summer 2023",
-      img: "http://i0.hdslb.com/bfs/archive/917f7d98f73cb7af240dacc6bdfe160ff60c1c31.jpg",
-      owner_mid: 1794123514,
-      owner_name: "robertmin96",
-      view: 7,
-      like: 0,
-      star: 0,
-      share: 1,
-      comment: 0,
-      disable: false,
-      checked: false,
-    },
-  ],
-  sets: [
-    {
-      id: 3737408,
-      title: "list3",
-      video_ids: ["BV1LC4y1Z7Li"],
-    },
-    {
-      id: 3737407,
-      title: "list2",
-      video_ids: ["BV1y8411C7Mw"],
-    },
-    {
-      id: 3670927,
-      title: "list1",
-      video_ids: ["BV1LC4y1Z7Li", "BV1y8411C7Mw"],
-    },
-  ],
-  likes: [
-    {
-      id: "BV1LC4y1Z7Li",
-      bvid: "BV1LC4y1Z7Li",
-      aid: 789284286,
-      cid: 1291466320,
-      title: "Foggy Brown",
-      desc: "Bronw University Main Green on a foggy data",
-      img: "http://i2.hdslb.com/bfs/archive/4632e780d3411dccd8fd6758e7d39968449cebf5.jpg",
-      owner_mid: 1794123514,
-      owner_name: "robertmin96",
-      view: 6,
-      like: 0,
-      star: 0,
-      share: 0,
-      comment: 1,
-      disable: true,
-      checked: false,
-    },
-    {
-      id: "BV1y8411C7Mw",
-      bvid: "BV1y8411C7Mw",
-      aid: 234207717,
-      cid: 1285040206,
-      title: "Tennis Practice",
-      desc: "I played tennis with friend in summer 2023",
-      img: "http://i0.hdslb.com/bfs/archive/917f7d98f73cb7af240dacc6bdfe160ff60c1c31.jpg",
-      owner_mid: 1794123514,
-      owner_name: "robertmin96",
-      view: 7,
-      like: 0,
-      star: 0,
-      share: 1,
-      comment: 0,
-      disable: false,
-      checked: false,
-    },
-  ],
-  follow: [
-    { id: "1", name: "xxx", img: "img", disable: false, checked: false },
-    { id: "2", name: "yyy", img: "img", disable: true, checked: true },
-    { id: "3", name: "zzz", img: "img", disable: false, checked: false },
-  ],
-};
+// let data = {
+//   videos: [
+//     {
+//       id: "BV1LC4y1Z7Li",
+//       bvid: "BV1LC4y1Z7Li",
+//       aid: 789284286,
+//       cid: 1291466320,
+//       title: "Foggy Brown",
+//       desc: "Bronw University Main Green on a foggy data",
+//       img: "http://i2.hdslb.com/bfs/archive/4632e780d3411dccd8fd6758e7d39968449cebf5.jpg",
+//       owner_mid: 1794123514,
+//       owner_name: "robertmin96",
+//       view: 6,
+//       like: 0,
+//       star: 0,
+//       share: 0,
+//       comment: 1,
+//       disable: true,
+//       checked: false,
+//     },
+//     {
+//       id: "BV1y8411C7Mw",
+//       bvid: "BV1y8411C7Mw",
+//       aid: 234207717,
+//       cid: 1285040206,
+//       title: "Tennis Practice",
+//       desc: "I played tennis with friend in summer 2023",
+//       img: "http://i0.hdslb.com/bfs/archive/917f7d98f73cb7af240dacc6bdfe160ff60c1c31.jpg",
+//       owner_mid: 1794123514,
+//       owner_name: "robertmin96",
+//       view: 7,
+//       like: 0,
+//       star: 0,
+//       share: 1,
+//       comment: 0,
+//       disable: false,
+//       checked: false,
+//     },
+//   ],
+//   sets: [
+//     {
+//       id: 3737408,
+//       title: "list3",
+//       video_ids: ["BV1LC4y1Z7Li"],
+//     },
+//     {
+//       id: 3737407,
+//       title: "list2",
+//       video_ids: ["BV1y8411C7Mw"],
+//     },
+//     {
+//       id: 3670927,
+//       title: "list1",
+//       video_ids: ["BV1LC4y1Z7Li", "BV1y8411C7Mw"],
+//     },
+//   ],
+//   likes: [
+//     {
+//       id: "BV1LC4y1Z7Li",
+//       bvid: "BV1LC4y1Z7Li",
+//       aid: 789284286,
+//       cid: 1291466320,
+//       title: "Foggy Brown",
+//       desc: "Bronw University Main Green on a foggy data",
+//       img: "http://i2.hdslb.com/bfs/archive/4632e780d3411dccd8fd6758e7d39968449cebf5.jpg",
+//       owner_mid: 1794123514,
+//       owner_name: "robertmin96",
+//       view: 6,
+//       like: 0,
+//       star: 0,
+//       share: 0,
+//       comment: 1,
+//       disable: true,
+//       checked: false,
+//     },
+//     {
+//       id: "BV1y8411C7Mw",
+//       bvid: "BV1y8411C7Mw",
+//       aid: 234207717,
+//       cid: 1285040206,
+//       title: "Tennis Practice",
+//       desc: "I played tennis with friend in summer 2023",
+//       img: "http://i0.hdslb.com/bfs/archive/917f7d98f73cb7af240dacc6bdfe160ff60c1c31.jpg",
+//       owner_mid: 1794123514,
+//       owner_name: "robertmin96",
+//       view: 7,
+//       like: 0,
+//       star: 0,
+//       share: 1,
+//       comment: 0,
+//       disable: false,
+//       checked: false,
+//     },
+//   ],
+//   follow: [
+//     { id: "1", name: "xxx", img: "img", disable: false, checked: false },
+//     { id: "2", name: "yyy", img: "img", disable: true, checked: true },
+//     { id: "3", name: "zzz", img: "img", disable: false, checked: false },
+//   ],
+// };
 
 export function B2YViewer() {
-  // const [data, setData] = useState(null);
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     await fetch(
-  //       `http://localhost:8000/B2Y/viewer?SESSDATA=${sessionStorage.getItem(
-  //         "SESSDATA"
-  //       )}&access_token=${sessionStorage.getItem("access_token")}`,
-  //       { method: "get" }
-  //     )
-  //       .then(async (res) => await res.json())
-  //       .then((json) => {
-  //         let data = json.data;
-  //         if (data.sets === null) data.sets = [];
-  //         if (data.likes === null) data.likes = [];
-  //         if (data.follow === null) data.follow = [];
-  //         setData(data);
-  //         setCheckSetList(Array(data.sets.length).fill([]));
-  //       });
-  //   };
-  //   fetchData();
-  // }, []);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(
+        `http://localhost:8000/B2Y/viewer?SESSDATA=${sessionStorage.getItem(
+          "SESSDATA"
+        )}&access_token=${sessionStorage.getItem("access_token")}`,
+        { method: "get" }
+      )
+        .then(async (res) => await res.json())
+        .then((json) => {
+          let data = json.data;
+          if (data.sets === null) data.sets = [];
+          if (data.likes === null) data.likes = [];
+          if (data.follow === null) data.follow = [];
+          setData(data);
+          setCheckSetList(Array(data.sets.length).fill([]));
+        });
+    };
+    fetchData();
+  }, []);
 
   const [checkSetList, setCheckSetList] = useState(
-    Array(data.sets.length).fill([])
-    // []
+    // Array(data.sets.length).fill([])
+    []
   );
   const [checkLikesList, setCheckLikesList] = useState([]);
   const [checkFollowList, setCheckFollowList] = useState([]);
@@ -203,6 +203,8 @@ export function B2YViewer() {
         body: JSON.stringify({
           sets: tmp.map((e, i) => ({
             setid: data.sets[i].id,
+            name: data.sets[i].name,
+            desc: data.sets[i].desc,
             videos: e,
           })),
           likes: likes,
@@ -221,9 +223,9 @@ export function B2YViewer() {
   };
 
   if (
-    // sessionStorage.getItem("SESSDATA") == null ||
-    // sessionStorage.getItem("access_token") == null
-    false
+    sessionStorage.getItem("SESSDATA") == null ||
+    sessionStorage.getItem("access_token") == null
+    // false
   ) {
     return <Navigate replace to="/login" />;
   } else {
